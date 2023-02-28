39572661648365c8443a4394f942fa83c633072a
UrosD <urosdelic89@gmail.com>
2023-02-28 14:11:50 +0100

    installed cors and renamed server to index

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const routes = require("../routes/user");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use("/api", routes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
