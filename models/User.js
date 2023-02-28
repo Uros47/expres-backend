4c13d21a5976d1e2f8980699adbd633beb5a159b
UrosD <urosdelic89@gmail.com>
2023-02-28 14:24:08 +0100

    User model redone

--- models/User.js
+++ models/User.js
@@ -1,25 +1,12 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const RegisterSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, "First Name is required"],
-  },
-  lastName: {
-    type: String,
-    required: [true, "Last Name is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-});
-
-const User = mongoose.model("user", RegisterSchema);
-
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+  })
+);
 module.exports = User;
